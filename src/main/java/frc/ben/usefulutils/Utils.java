f2b98109c290168aece228403f3b445afa9af73b
Bhunt-2 <Benh902@stemia.ca>
2026-08-22 12:03:21 -0600

    test files

---
+++ src/main/java/frc/ben/usefulutils/Utils.java
@@ -0,0 +1,22 @@
+package frc.ben.usefulutils;
+
+public final class Utils {
+
+    private Utils() {}
+
+    public static double clamp(double value, double min, double max) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static double deadband(double value, double deadband) {
+        if (Math.abs(value) < deadband) {
+            return 0.0;
+        }
+
+        return value;
+    }
+
+    public static double squareInput(double value) {
+        return Math.copySign(value * value, value);
+    }
+}
